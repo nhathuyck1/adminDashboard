934543d929aca947c6dbadf80d892790f8fc41cf
Hajime <dangnhathuy61@gmail.com>
2025-09-15 16:35:56 +0700

    Fetch Order to dashboard

--- src/page/order/index.jsx
+++ src/page/order/index.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Table } from "antd";
+import React, { use, useEffect, useState } from "react";
+import axios from "axios";
 
 const ManageOrder = () => {
-  return <div>ManageOrder</div>;
+  const [orders, setOrder] = useState([]);
+  const column = [
+    {
+      title: "OrderCode",
+      dataIndex: "OrderCode",
+      key: "orderCode",
+    },
+    {
+      title: "CustomerName",
+      dataIndex: "CustomerName",
+      key: "customerName",
+    },
+    {
+      title: "CarId",
+      dataIndex: "CarId",
+      key: "carId",
+    },
+    {
+      title: "CreateAt",
+      dataIndex: "CreateAt",
+      key: "createAt",
+    },
+  ];
+  const fetchOrder = async () => {
+    const response = await axios.get(
+      "https://68c7ae0b5d8d9f51473290a9.mockapi.io/order"
+    );
+    console.log(response.data);
+    setOrder(response.data);
+  };
+  useEffect(() => {
+    fetchOrder();
+  }, []);
+  return (
+    <>
+      <Table dataSource={orders} columns={column} />
+    </>
+  );
 };
-
 export default ManageOrder;
